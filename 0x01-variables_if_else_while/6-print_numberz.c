d75d32bc64db2555e6c022e96befeddcd34f1ffb
Your Name <you@example.com>
2023-03-17 13:45:10 -0700

    the numbers as character

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+/**
+ * main - function for printing alphabet
+ * Return: Alwayas (Success)
+ */
+int main(void)
+{
+int count = 48;
+for (; count <= 57; count++)
+{
+putchar(count);
+}
+putchar('\n');
+return (0);
+}
