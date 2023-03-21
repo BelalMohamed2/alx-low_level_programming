b73bbcc9d555115aa180078f3ddafe06c70eb239
Your Name <you@example.com>
2023-03-21 08:11:51 -0700

    find the sign

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,21 @@
+#include "main"
+/**
+ * print_sign - print the sing of the input
+ * @c: the input to be checked
+ * Return: 1 if greater zero 0 if zero -1 if negative
+ */
+int print_sign(int n)
+{
+if (n < 0)
+{
+	return (-1);
+}
+else if (n = 0)
+{
+	return (0);
+}
+else
+{
+	return (1);
+}
+}
