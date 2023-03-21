23741538da02fb7c03fc0f09cefe665f711db223
Your Name <you@example.com>
2023-03-21 06:45:10 -0700

    header file importance in c

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/*
+ * main - Entry point
+ * description: function for printing the word putchar
+ * Return: Always(success)
+ *
+ */
+int main(void)
+{
+_putchar('_');
+_putchar('p');
+_putchar('u');
+_putchar('t');
+_putchar('c');
+_putchar('h');
+_putchar('a');
+_putchar('r');
+_putchar('\n');
+return (0);
+}
