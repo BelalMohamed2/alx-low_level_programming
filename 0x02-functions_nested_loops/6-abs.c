1f151bf472de4ee35e29fd7f55d34e044edcdaad
Your Name <you@example.com>
2023-03-21 08:28:52 -0700

    getting the absolute

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * _abs - for finding the abslute value of the input
+ * @i: the input of the function
+ * Return: the abslute values
+ */
+int _abs(int i)
+{
+	if (i >= 0)
+	{
+		return (i);
+	}
+	else
+	{
+		return (-1 * i);
+	}
+}
