57f4040cd4fc8e519f8ad0e59ec89cf70cba963c
Your Name <you@example.com>
2023-03-21 06:59:02 -0700

    betty style description problem sloving

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,13 +1,12 @@
 #include "main.h"
-/*
+/**
  * main - Entry point
- * Description: function for printing the word putchar
+ * Description: 'function for printing the word putchar'
  * Return: Always(success)
- *
  */
 int main(void)
 {
-/* function for printing putchar char by char*/
+/* Description: 'function for printing the word putchar'*/
 _putchar('_');
 _putchar('p');
 _putchar('u');
