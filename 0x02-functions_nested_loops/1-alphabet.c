9377778051f6e37d6552b119ddf225a7cac4ff7e
Your Name <you@example.com>
2023-03-21 07:29:41 -0700

    printing alphabets

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * main - Entry point
+ * Description: 'prints alphabet in lower case'
+ * Return: Always (Success)
+ */
+void print_alphabet(void)
+{
+int i = 97;
+for (;i <= 122; i++)
+{
+_putchar(i);
+}
+_putchar('\n');
+}
