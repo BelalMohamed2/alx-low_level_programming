e0744d51675ebf5ef9a49558dde7ab50667966bd
Your Name <you@example.com>
2023-03-21 07:39:09 -0700

    betty style resolving problem

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,12 +1,11 @@
 #include "main.h"
 /**
- * main - Entry point
+ * print_alphabet - Entry point
  * Description: 'prints alphabet in lower case'
  * Return: Always (Success)
  */
 void print_alphabet(void)
 {
-/* Description: 'function for printing the alphabets'*/
 int i = 97;
 for (; i <= 122; i++)
 {
