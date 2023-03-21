0efe445374dd36d66253d87b96e907bf511592df
Your Name <you@example.com>
2023-03-21 13:40:22 -0700

    new algo

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -6,17 +6,31 @@
  */
 void times_table(void)
 {
-	int a, b, c;
+	int a, b, c, d;
 
 	for (a = 0; a <= 9; a++)
 	{
 		for (b = 0; b <= 9; b++)
 		{
 			c = a * b;
-			_putchar(c + 48);
-			_putchar(',');
-			_putchar(' ');
-			_putchar(' ');
+			if (c == 0 && b == 0)
+				_putchar(z + '0');
+			else if (c > 9)
+			{
+				d = c % 10;
+				c = c / 10;
+				_putchar(',');
+				_putchar(' ');
+				_putchar(c + '0');
+				_putchar(d + '0');
+			}
+			else
+			{
+				_putchar(',');
+				_putchar(' ');
+				_putchar(' ');
+				_putchar(d + '0');
+			}
 		}
 		_putchar('\n');
 	}
