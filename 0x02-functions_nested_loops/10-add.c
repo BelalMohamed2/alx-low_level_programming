6bc74c526750e98348a6f00b78cdf2d0056f5db1
Your Name <you@example.com>
2023-03-21 13:49:44 -0700

    adding variables

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,11 @@
+#include "main.h"
+/**
+ * add - adding two values
+ * @i: variable one
+ * @j: variable two
+ * Return: summation of the two values
+ */
+int add(int i, int j)
+{
+	return (i + j);
+}
