cb6c7d3eed44169d47e990582d0feae64d838ce1
Your Name <you@example.com>
2023-03-23 15:57:54 -0700

    printing diagonal

---
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+ * print_diagonal - printing spaces as diagonal shape
+ * @n: the argument of the function
+ * return: always success
+ */
+void print_diagonal(int n)
+{
+	int i, j;
+
+	for (i = 1; i <= n; i++)
+	{
+		for (j = 1; j <= n - 1; j++)
+		{
+			_putchar(' ');
+		}
+		_putchar('\\');
+	}
+	_putchar('\n');
+}
