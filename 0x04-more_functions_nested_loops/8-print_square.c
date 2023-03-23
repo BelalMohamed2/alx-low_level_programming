45c3033045d595d22482b1741a9e1db42692831a
Your Name <you@example.com>
2023-03-23 16:22:43 -0700

    error handling

--- 0x04-more_functions_nested_loops/8-print_square.c
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -8,12 +8,19 @@ void print_square(int size)
 {
 	int i, j;
 
-	for (i = 0; i <= size - 1; i++)
+	if (size <= 0)
 	{
-		for (j = 0; j <= size - 1; j++)
+		_putchar('\n');
+	}
+	else
+	{
+	for (i = 0; i < size; i++)
+	{
+		for (j = 0; j < size; j++)
 		{
 			_putchar('#');
 		}
 		_putchar('\n');
+	}
 	}
 }
