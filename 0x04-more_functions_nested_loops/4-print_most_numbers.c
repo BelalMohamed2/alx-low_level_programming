22303020d3dd1f000d5be44bf7d9ff47379df523
Your Name <you@example.com>
2023-03-23 14:42:17 -0700

    fixing problems

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -7,11 +7,15 @@ void print_most_numbers(void)
 {
 	int i;
 
-	for (i = 0; i <= 9; i++)
+	for (i = 48; i <= 57; i++)
 	{
-		if (i == 2 || i == 4)
+		if (i == 50 || i == 52)
 		{
-			_putchar(i + '0');
+			continue;
+		}
+		else
+		{
+			_putchar(i);
 		}
 	}
 	_putchar('\n');
