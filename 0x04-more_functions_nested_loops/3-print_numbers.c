bd2239199d5fc58042047a3bf5a05bab1f5f283c
Your Name <you@example.com>
2023-03-23 14:11:18 -0700

    new commiting of the

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * print_numbers - print numbers as characters
+ * return: no values returned
+ */
+void print_numbers(void)
+{
+	int i;
+
+	for (i = 0; i <= 57; i++)
+	{
+		_putchar(i);
+	}
+	_putchar('\n');
+}
