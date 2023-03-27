e2c48eff52258d69b4ca93c613780297bc90fce1
Your Name <you@example.com>
2023-03-27 07:32:12 -0700

    change value by derefrencing

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,10 @@
+#include "main.h"
+/**
+ * reset_to_98 - modify the value of the int using poiter
+ * @n: is the pointer to variable n
+ * Return: always 0
+ */
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
