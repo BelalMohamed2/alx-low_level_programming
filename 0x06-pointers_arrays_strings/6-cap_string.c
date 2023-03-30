26e99cb49033e255429c7f33ae761ad84bf1f460
Your Name <you@example.com>
2023-03-30 15:11:10 -0700

    capitilizing the strings

---
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -0,0 +1,30 @@
+#include "main.h"
+/**
+ * cap_string - function to capitalize the words after a certain sperators
+ * @str: the input string
+ * Return: returned value is a ptr
+ */
+char *cap_string(char *str)
+{
+int i = 0;
+
+while (str[i] != '\0')
+{
+	if (i == 0 || str[i - 1] == ' ' || str[i - 1] == '\t' || str[i - 1] == '\n' || str[i - 1] == ',' || str[i - 1] == ';' || str[i - 1] == '.' || str[i - 1] == '!' || str[i - 1] == '?' || str[i - 1] == '"' || str[i - 1] == '(' || str[i - 1] == ')' || str[i - 1] == '{' || str[i - 1] == '}')
+{
+	if (str[i] >= 'a' && str[i] <= 'z')
+	{
+		str[i] = str[i] - 32;
+	}
+	else
+	{
+		if (str[i] >= 'A' && str[i] <= 'Z')
+			{
+				str[i] = str[i] + 32;
+			}
+	}
+}
+i++;
+}
+return (str);
+}
