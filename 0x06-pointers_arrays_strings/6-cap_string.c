30505dafa9e132c77fae515233c53d0d8a1fe8fa
Your Name <you@example.com>
2023-03-30 15:22:52 -0700

    identation betty problem

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -1,30 +1,36 @@
 #include "main.h"
 /**
- * cap_string - function to capitalize the words after a certain sperators
- * @str: the input string
- * Return: returned value is a ptr
+ * cap_string - function to capitalize words.
+ * @s: string.
+ * Return: capitalized string.
  */
-char *cap_string(char *str)
+char *cap_string(char *s)
 {
-int i = 0;
+	int i;
 
-while (str[i] != '\0')
-{
-	if (i == 0 || str[i - 1] == ' ' || str[i - 1] == '\t' || str[i - 1] == '\n' || str[i - 1] == ',' || str[i - 1] == ';' || str[i - 1] == '.' || str[i - 1] == '!' || str[i - 1] == '?' || str[i - 1] == '"' || str[i - 1] == '(' || str[i - 1] == ')' || str[i - 1] == '{' || str[i - 1] == '}')
-{
-	if (str[i] >= 'a' && str[i] <= 'z')
+	i = 0;
+	while (s[i] != '\0')
 	{
-		str[i] = str[i] - 32;
+		if ((s[i - 1] == ' ' || s[i - 1] == '\n'
+		|| s[i - 1] == '\t' || s[i - 1] == ','
+		|| s[i - 1] == ';' || s[i - 1] == '!'
+		|| s[i - 1] == '?' || s[i - 1] == '"'
+		|| s[i - 1] == '(' || s[i - 1] == ')'
+		|| s[i - 1] == '{' || s[i - 1] == '}'
+		|| s[i - 1] == '.')
+		&& (s[i] >= 'a' && s[i] <= 'z'))
+		{
+			s[i] = s[i] - 32;
+		}
+		else if ((s[0] >= 97 && s[0] <= 122))
+		{
+			s[0] = s[0] - 32;
+		}
+		else
+		{
+			s[i] = s[i];
+		}
+		i++;
 	}
-	else
-	{
-		if (str[i] >= 'A' && str[i] <= 'Z')
-			{
-				str[i] = str[i] + 32;
-			}
-	}
-}
-i++;
-}
-return (str);
+	return (s);
 }
