a2360883668f1e9faa9b77d2049a0d2466d265e0
Your Name <you@example.com>
2023-04-03 17:37:00 -0700

    character location in string

---
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,22 @@
+#include "main.h"
+#include <stddef.h>
+/**
+ * _strchr - function used to locate a character inside a string
+ * @c: character where we search for
+ * @s: a pointer to an array of strings
+ * Return: loctaion if founded and 0 if not found
+ */
+char *_strchr(char *s, char c)
+{
+	int i = 0;
+
+	while (s[i])
+	{
+		if (s[i] == c)
+		{
+			return (&s[i]);
+		}
+		i++;
+	}
+return  (NULL);
+}
