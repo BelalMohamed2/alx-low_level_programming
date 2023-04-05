411de23913a6420652f1392e75d193f968f3d977
Your Name <you@example.com>
2023-04-05 13:01:35 -0700

    find sqrt using recursion

---
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -0,0 +1,29 @@
+#include "main.h"
+/**
+ * _sqrt_recursion - function to find the square root of input value
+ * @n: the input intger to the function
+ * Return: the value of the square root
+ */
+int _sqrt_recursion(int n)
+{
+	return (_square_root(n, 1));
+}
+/**
+ * _square_root - a helping function that have the recursive provalbilty
+ * @n: the input intger
+ * @i: the intger we recusive with
+ * Return: the square root of the number
+ */
+int _square_root(int n, int i)
+{
+	if (i == n)
+		return (-1);
+	if (n == 1 || n == 0)
+		return (n);
+	if (i == n / i)
+	{
+		return (i);
+	}
+	return (_square_root(n, i + 1));
+}
+
