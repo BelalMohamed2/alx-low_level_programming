92165cee33b3152312d2fcc8c3bc612842f6442e
Your Name <you@example.com>
2023-04-07 07:22:18 -0700

    string first occurance

---
+++ 0x07-pointers_arrays_strings/4-strpbrk.c
@@ -0,0 +1,29 @@
+#include "main.h"
+/**
+ * _strpbrk - function locates the first occurance of the string s in accept
+ * @s: a pointer in the array to search for
+ * @accept: a pointer for the array to search in
+ * Return: returning a pointer to the matching string if there is a match
+ */
+char *_strpbrk(char *s, char *accept)
+{
+	char *location;
+	int i, j;
+
+	for (i = 0; accept[i] != '\0'; i++)
+	{
+		if accept[i] == s[0]
+		{
+			location = accept + i;
+			for (j = 0; s[j] != '\0'; j++)
+			{
+				if s[j] != accept[i + j]
+				{
+					return ('\0');
+				}
+			}
+			return (location);
+		}
+	}
+	return ('\0');
+}
