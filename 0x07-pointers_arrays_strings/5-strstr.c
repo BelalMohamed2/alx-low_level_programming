26cea8e0edad7892092494290ca6bb98970a77c0
Your Name <you@example.com>
2023-04-07 08:11:18 -0700

    find the occurance in the file in another file

---
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -0,0 +1,31 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * _strstr - function locates the first occurance of the string needle
+ * in haystack string
+ * @needle : a pointer in the array to search for
+ * @haystack: a pointer for the array to search in
+ * Return: returning a pointer to the matching string if there is a match
+ */
+char *_strstr(char *haystack, char *needle)
+{
+	char *location;
+	int i, j;
+
+	for (i = 0; haystack[i] != '\0'; i++)
+	{
+		if (needle[i] == haystack[0])
+		{
+			location = haystack + i;
+			for (j = 0; needle[j] != '\0'; j++)
+			{
+				if (needle[j] != haystack[i + j])
+				{
+					return (NULL);
+				}
+			}
+			return (location);
+		}
+	}
+	return (NULL);
+}
