dd829310d21de916ead006d79d7fe567488af8d2
Your Name <you@example.com>
2023-04-10 14:05:21 -0700

    adding arguments

---
+++ 0x0A-argc_argv/4-add.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main - function that is used for adding positive numbers
+ * @argc: the number of input arguments
+ * @argv: the argument vector
+ * Return: 0 if the summation without any letters shown and if there is letter
+ */
+int main(int argc, char *argv[])
+{
+	int sum = 0, i = 0, var;
+
+	if (argc > 1)
+	{
+		for (; i < argc; i++)
+		{
+			if (argv[i] >= '0' && argv[i] <= '9')
+			{
+				var = atoi(argv[i]);
+				sum += var;
+			}
+			else
+			{
+				printf("Error\n");
+				return (1);
+			}
+		}
+		printf("%d\n", sum);
+		return (0);
+	}
+	printf("0\n");
+	return (0);
+}
