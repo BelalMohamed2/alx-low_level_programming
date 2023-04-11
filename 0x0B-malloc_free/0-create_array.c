b128b7b80d81c99469a564f61d423d67901c3cad
Your Name <you@example.com>
2023-04-11 08:34:01 -0700

    uploading all files

---
+++ 0x0B-malloc_free/0-create_array.c
@@ -0,0 +1,25 @@
+#include "main.h"
+#include <stdlib.h>
+/**
+ * create_array - function used to create an array
+ * @size: the size of the array
+ * @c: charater used as an intialization of the string
+ * Return: NULL if size is 0 and a pointer to array if not
+ */
+char *create_array(unsigned int size, char c)
+{
+	char *ptr;
+	unsigned int i;
+
+	if (size == 0)
+	{
+		return (NULL);
+	}
+	ptr = malloc(size * sizeof(char));
+	for (i = 0; i < size; i++)
+	{
+		ptr[i] = c;
+	}
+	return (ptr);
+}
+
