f844665a5c65b29fe6fe6f12288e7da39ba32a78
Your Name <you@example.com>
2023-04-13 14:18:25 -0700

    array of numbers sorted

--- 0x0C-more_malloc_free/3-array_range.c
+++ 0x0C-more_malloc_free/3-array_range.c
@@ -13,8 +13,8 @@ int *array_range(int min, int max)
 
 	if (min > max)
 		return (NULL);
-	ptr = malloc((max - min) * sizeof(int));
-	if (!ptr)
+	ptr = malloc((max - min + 1) * sizeof(int));
+	if (ptr == NULL)
 		return (NULL);
 	for (i = min; i <= max; i++)
 	{
