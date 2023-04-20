cbd13fe329ce17d1abff0585a0e4ba6837568467
Your Name <you@example.com>
2023-04-20 08:25:51 -0700

    summing undefine number of inputs

--- 0x10-variadic_functions/0-sum_them_all.c
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -1,4 +1,5 @@
 #include "variadic_functions.h"
+#include <stdarg.h>
 /**
  * sum_them_all -  function that returns the sum of all its parameters.
  * @n: the count of the inputs
@@ -7,9 +8,10 @@
 int sum_them_all(const unsigned int n, ...)
 {
 	va_list num;
-	int i = 0, sum = 0;
+	unsigned int i = 0;
+	int sum = 0;
 
-	va_start(n);
+	va_start(num, n);
 	if (n == 0)
 		return (0);
 	for (; i < n; i++)
