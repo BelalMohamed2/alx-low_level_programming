d30f2c1e524ca6994488155ca738ae31d78d7015
BelalMohamed2 <belalmohamed.abdelkreem02@eng-st.cu.edu.eg>
2023-04-30 03:28:09 -0700

    freeing memory

---
+++ 0x13-more_singly_linked_lists/4-free_listint.c
@@ -0,0 +1,20 @@
+#include "lists.h"
+/**
+ * free_listint - function that frees a listint_t list.
+ * @head: is a pointer to the start of the linked list
+ * Return: no returned value
+ */
+void free_listint(listint_t *head)
+{
+	listint_t *temp = head;
+	if (head == NULL)
+		return;
+
+	while (head->next != NULL)
+	{
+		temp = head;
+		head = head->next;
+		free(temp);
+	}
+	free(head);
+}
