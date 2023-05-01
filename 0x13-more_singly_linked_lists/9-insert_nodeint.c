eaf179d712dd47e89becac151a92361a529f6018
BelalMohamed2 <belalmohamed.abdelkreem02@eng-st.cu.edu.eg>
2023-05-01 05:45:58 -0700

    inserting code

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -25,7 +25,7 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 	while ((idx - 1) > 0)
 	{
 		temp = temp->next;
-		if (!temp)
+		if (!temp || !temp->next)
 			return (NULL);
 		idx--;
 	}
