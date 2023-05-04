887fbfe62dde447cc396d5dd2ead74adee3a2637
BelalMohamed2 <belalmohamed.abdelkreem02@eng-st.cu.edu.eg>
2023-05-04 05:29:21 -0700

    adding files

--- 0x14-bit_manipulation/second_slou_P.c
+++ 0x14-bit_manipulation/second_slou_P.c
@@ -7,7 +7,7 @@
 unsigned int binary_to_uint(const char *b)
 {
 	unsigned int output = 0;
-	int i = 0, len = 0;
+	int i = 0;
 
 	if (b == NULL)
 		return (0);
@@ -15,13 +15,11 @@ unsigned int binary_to_uint(const char *b)
 	{
 		if (b[i] != 48 && b[i] != 49)
 			return (0);
+		if (b[i] == '1')
+			output = output | (1 << 0);
+		output = output << 1;
 		i++;
 	}
-	len = --i;
-	for (; b[i]; i--)
-	{
-		if (b[i] == '1')
-			output = output | (1 << (len - i));
-	}
+	output = output >> 1;
 	return (output);
 }
