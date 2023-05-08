403117d17f21bed9a72be9cc15ca2fb53e0357aa
BelalMohamed2 <belalmohamed.abdelkreem02@eng-st.cu.edu.eg>
2023-05-08 13:04:12 -0700

    updat

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -16,7 +16,7 @@ int main(int argc, char *argv[])
 		dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n");
 		exit(97);
 	}
-	fread= open(argv[1], O_RDONLY);
+	fread = open(argv[1], O_RDONLY);
 	if (fread < 0)
 	{
 		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
